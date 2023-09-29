020b3bad656202e2ac0d8b7f0fce23d8b3ebd649
Autumn Han <autumn9894@gmail.com>
2023-09-29 16:04:03 -0700

    acronym function working

--- strings_I.js
+++ strings_I.js
@@ -3,6 +3,7 @@ var testStr = "la l al ala l a";
 var testStrTwo = "I can not BELIEVE it's not BUTTER";
 var testStrThree = "abc8c0d1ngd0j0!8";
 var testStrFour = "0s1a3y5w7h9a2t4?6!8?0";
+var testStrFive = "Live from New York, it's Saturday Night!";
 
 // REMOVE BLANKS
 function removeBlanks(str) {
@@ -29,3 +30,16 @@ function getDigits(str) {
 }
 // getDigits(testStrThree);
 // getDigits(testStrFour);
+
+// ACRONYMS
+function acronym(str) {
+  var words = str.split(" ");
+  var newStr = "";
+  for (var i = 0; i < words.length; i++) {
+    var chars = words[i].split("");
+    newStr += chars[0];
+  }
+  console.log(newStr.toUpperCase());
+}
+// acronym(testStrFive);
+// acronym(testStrTwo);
