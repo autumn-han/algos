63a6cf2067eae14d48deb0aed4d8245edbcf2407
Autumn Han <autumn9894@gmail.com>
2023-09-29 15:23:47 -0700

    getDigits function working

--- strings_I.js
+++ strings_I.js
@@ -1,6 +1,8 @@
 // TEST VARIABLES
 var testStr = "la l al ala l a";
 var testStrTwo = "I can not BELIEVE it's not BUTTER";
+var testStrThree = "abc8c0d1ngd0j0!8";
+var testStrFour = "0s1a3y5w7h9a2t4?6!8?0";
 
 // REMOVE BLANKS
 function removeBlanks(str) {
@@ -14,3 +16,16 @@ function removeBlanks(str) {
 }
 // console.log(removeBlanks(testStr));
 // console.log(removeBlanks(testStrTwo));
+
+// GET DIGITS
+function getDigits(str) {
+  var newStr = "";
+  for (var i = 0; i < str.length; i++) {
+    if (!isNaN(str[i])) {
+      newStr += str[i];
+    }
+  }
+  console.log(newStr);
+}
+// getDigits(testStrThree);
+// getDigits(testStrFour);
