09620cec57d98acd1b9b6a91df3a2293d0af9948
Autumn Han <autumn9894@gmail.com>
2023-10-10 17:22:44 -0700

    finished front assignment

---
+++ front.js
@@ -0,0 +1,57 @@
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+class SLL1 {
+  constructor() {
+    this.head = null;
+  }
+  // ADD FRONT
+  addFront(val) {
+    // creating a new Node object
+    let new_node = new Node(val);
+    // check to see if the current list does not have a head node (i.e. the list is empty)
+    if (!this.head) {
+      // if the list is empty ---> place the new Node as the head
+      this.head = new_node;
+      return this;
+    }
+    // otherwise ---> assign the head to be the next node to the new Node
+    new_node.next = this.head;
+    // then assign the new_node to be the new head of the list
+    this.head = new_node;
+    return this;
+  }
+  // REMOVE FRONT
+  removeFront() {
+    if (!this.head) {
+      return null;
+    }
+    let begone_head = this.head;
+    this.head = begone_head.next;
+    return this;
+  }
+  // FRONT
+  front() {
+    if (!this.head) {
+      return null;
+    }
+    return this.head.data;
+  }
+}
+// TESTING
+var mySLL1 = new SLL1();
+mySLL1.addFront(18);
+mySLL1.addFront(5);
+mySLL1.addFront(73);
+console.log(mySLL1);
+
+mySLL1.removeFront();
+console.log(mySLL1);
+mySLL1.removeFront();
+console.log(mySLL1);
+
+console.log(mySLL1.front());
